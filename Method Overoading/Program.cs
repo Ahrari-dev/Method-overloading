7d3a906fe1240e9e2b63fc5a5cf96a824c78a393
Hasib Ahrari <hasibahrari@gmail.com>
2022-05-12 16:29:07 -0700

    Final commit

---
+++ Method Overoading/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MethodsExcercise
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //{
+            //    Add(1, 1);
+            //    Add(2.2, 3.3);
+            //}
+            //public static void Add(int x, int y)
+            //{
+            //    int answer = x + y;
+            //    Console.WriteLine(answer);
+            //}
+            //public static void Add(double x, double y)
+            //{
+            //    double answer = x + y;
+            //    Console.WriteLine(answer);
+            //}
+
+            Add(1, 4, true);
+
+            Console.ReadKey(true);
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool showResult)
+        {
+            var result = Add(a, b);
+
+            if (showResult)
+            {
+                Console.WriteLine(result);
+            }
+
+            return result.ToString();
+        }
+    }
+}
